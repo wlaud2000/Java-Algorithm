5bf691c2e773431fceadf7da3852bca55d950e7d
Jimyeong-Kim <86175262+wlaud2000@users.noreply.github.com>
2026-02-08 10:30:06 +0900

    [Bronze III] Title: 직각삼각형, Time: 88 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,41 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       
- 
+        
         while(true) {
-        	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             
-        	int x = Integer.parseInt(st.nextToken());
-        	int y = Integer.parseInt(st.nextToken());
-        	int z = Integer.parseInt(st.nextToken());
-        	
-        	
-			// 0 0 0 을 입력받으면 종료
-        	if(x == 0 && y == 0 && z == 0) break;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
             
-        	
-        	if((x * x + y * y) == z * z) {
-				System.out.println("right");
-			}
-        	else if(x * x == (y * y + z * z)) {
-				System.out.println("right");
-			}
-        	else if(y * y == (z * z + x * x)) {
-				System.out.println("right");
-			}
-        	else {
-				System.out.println("wrong");
-			}
-        	
-		}
- 
-	}
- 
+            if (a==0 && b==0 && c==0) {
+                break;
+            }
+            
+            if (a*a == (b*b + c*c)) {
+                System.out.println("right");
+            } else if (b*b == (a*a + c*c)) {
+                System.out.println("right");
+            } else if (c*c == (a*a + b*b)) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
+    }
 }
