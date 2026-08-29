967349de8c531a69123caf3742d50347d8c5ff63
Jimyeong-Kim <86175262+wlaud2000@users.noreply.github.com>
2026-08-29 20:40:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.82 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,20 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
+        StringBuilder sb = new StringBuilder();
         HashMap<String, Integer> map = new HashMap<>();
         
-        // participant 순회하면서 등장 횟수 카운트
-        for (String p: participant) {
-            map.put(p, map.getOrDefault(p,0) + 1); // key가 map에 있으면 그 value를 없으면 0을 줌
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        // completion 순회하면서 카운트 차감
-        for (String c: completion) {
+        for(String c : completion) {
             map.put(c, map.get(c) - 1);
         }
         
-        for (String key: map.keySet()) { // map.keySet()은 HashMap에 있는 모든 key를 Set으로 반환해주는 메서드
-            if (map.get(key) > 0) {
+        for(String key : participant) {
+            if(map.get(key) > 0) {
                 return key;
             }
         }
