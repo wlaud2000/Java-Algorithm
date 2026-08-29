bc94f9eeec8ba0fd76a0ba4daafef9f2ac578158
Jimyeong-Kim <86175262+wlaud2000@users.noreply.github.com>
2026-08-29 21:05:57 +0900

    [level 1] Title: 폰켓몬, Time: 3.65 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,31 @@
+// 홍박사님 포켓몬 N/2마리 가져가기
+// 가질 수 있는 최댓값 조합 찾기
+// 해시맵? 해시셋? 해시맵으로가자 Integer , Integer
+// nums를 돌면서 key(포켓몬 번호) 나오면 value를 +1해주기?
+// 일단 nums의 lenght/2가 최대임 int limit = nums.length / 2
+// 그래서 맵의 key의 개수 > limit 이면 limit리턴, key의 개수 < limit이면 key의 개수 리턴
+// 그럼 set으로 해도 되는거 아닌가? set으로 하자
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // 해시셋 선언
+        HashSet<Integer> set = new HashSet<>();
         
-        int max = nums.length/2;
-        
-        for (Integer n: nums) {
-            map.put(n, map.getOrDefault(n,0)+1);
+        // nums 돌면서 해시셋 채워주기 
+        for(int key : nums) {
+            set.add(key);
         }
         
-        int ans = map.size();
+        // limit 선언
+        int limit = nums.length/2;
         
-        if (ans > max) {
-            return max;
+        // key의 개수 > limit 이면 limit리턴, key의 개수 < limit이면 key의 개수 리턴
+        if(set.size() > limit) {
+            return limit;
         } else {
-            return ans;
+            return set.size();
         }
     }
 }
